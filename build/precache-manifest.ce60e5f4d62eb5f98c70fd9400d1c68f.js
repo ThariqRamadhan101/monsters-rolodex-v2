207997361858f8d516316d7a7462574f54f45942
thariqramadhan <athariqramadhan@yahoo.com>
2019-12-27 11:59:38 +0700

    add file

---
+++ build/precache-manifest.ce60e5f4d62eb5f98c70fd9400d1c68f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c14dfb714b94e2ae1d0e91432b5d0e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "42eabdfd41eecef4bc3d",
+    "url": "/monsters-rolodex/static/css/main.10955e17.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42eabdfd41eecef4bc3d",
+    "url": "/monsters-rolodex/static/js/main.3b90c9f8.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
